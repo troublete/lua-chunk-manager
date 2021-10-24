5921d1667aea2e8c1c863acb7f5a3c1dec9a7d56
Willi Eßer <willi.esser@troublete.com>
2021-10-24 14:37:29 +0200

    updated install process

--- install.sh
+++ install.sh
@@ -7,12 +7,14 @@ mkdir -p $LCM_HOME/current/
 echo 'fetching latest lcm'
 curl -u 'troublete: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' -sL 'https://github.com/troublete/lua-chunk-manager/tarball/master' > $LCM_HOME/current.tar
 
+
+
 echo 'extracting...'
 tar -xzf $LCM_HOME/current.tar -C $LCM_HOME/current/
 extracted=`ls $LCM_HOME/current/ | head -1`
 
 echo 'moving...'
-cp -r $LCM_HOME/current/$extracted/* ~/.lcm
+cp -r $LCM_HOME/current/$extracted/* $LCM_HOME
 
 echo 'cleaning...'
 rm $LCM_HOME/current.tar
@@ -20,11 +22,11 @@ rm -rf $LCM_HOME/current
 
 echo 'configuring shell...'
 if ! cat /etc/profile | grep '# load lcm config'; then
-	sudo bash -c "echo $'LCM_HOME=$LCM_HOME\nif [ -f $LCM_HOME/sh-config ]; then\nsource ~/.lcm/sh-config # load lcm config\nfi' >> /etc/profile"
+	sudo bash -c "echo $'\nLCM_HOME=$LCM_HOME\nif [ -f $LCM_HOME/sh-config ]; then\nsource $LCM_HOME/sh-config # load lcm config\nfi' >> /etc/profile"
 fi
 
 source $LCM_HOME/sh-config
 echo 'setting up lcm for usage...'
-cd $LCM_HOME && lua lcm.lua init -g && lua lcm.lua install -g
+lua $LCM_HOME/lcm.lua init -g && lua $LCM_Home/lcm.lua install -g
 
 echo $'done.\nYou might have to restart your terminal.'
